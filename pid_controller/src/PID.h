750892372d0ed938b7a448efebfd2e5b71ff19d3
Ella Schwarz <schwarzem@berkeley.edu>
2023-12-05 12:12:35 -0800

    Update pid_controller

--- pid_controller/src/PID.h
+++ pid_controller/src/PID.h
@@ -1,7 +1,7 @@
 #include <stdint.h>
 
-#define TIMESTEP 0.5
-#define TIMESTEP_LOG2 -1 //can be negative!!!
+#define TIMESTEP 1 // 0.5 second, inverted (=2), log2 (=1)
+#define TIMESTEP_D -1 //can be negative!!!
 
 typedef struct {
     //Parameters
@@ -20,5 +20,8 @@ typedef struct {
 
 } pid_controller;
 
-uint32_t update(pid_controller *pid, int32_t target, int32_t meas);
-uint32_t reset(pid_controller *pid);
+int32_t pid_update(pid_controller *pid, int32_t target, int32_t meas);
+void pid_init(pid_controller *pid, int32_t kp, int32_t ki, int32_t kd);
+void pid_reset(pid_controller *pid, int32_t initial_meas);
+//void pid_set_target(pid_controller *pid, int32_t target);
+int32_t fixedpt_mult(int32_t a, int32_t b);
