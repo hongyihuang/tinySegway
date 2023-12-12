728010ef57cb071d365dba6ab4d52b7b1349f0c6
Depetrol <lslrobert@outlook.com>
2023-12-11 18:12:34 -0800

    update sensor data

--- segway_sim/envs/segway.py
+++ segway_sim/envs/segway.py
@@ -7,7 +7,7 @@ from gymnasium.spaces import Box
 
 DEFAULT_CAMERA_CONFIG = {
     "trackbodyid": 0,
-    "distance": 2.04,
+    "distance": 1.5,
 }
 
 
@@ -18,14 +18,14 @@ class SegwayEnv(MujocoEnv, utils.EzPickle):
             "rgb_array",
             "depth_array",
         ],
-        "render_fps": 250,
+        "render_fps": 250
     }
 
     def __init__(self, max_ep_len = 1000, **kwargs):
         utils.EzPickle.__init__(self, **kwargs)
         self.step_count = 0
         self.max_ep_len = max_ep_len
-        observation_space = Box(low=-np.inf, high=np.inf, shape=(17,), dtype=np.float64)
+        observation_space = Box(low=-np.inf, high=np.inf, shape=(3,), dtype=np.float64)
         MujocoEnv.__init__(
             self,
             "segway.xml",
@@ -38,7 +38,7 @@ class SegwayEnv(MujocoEnv, utils.EzPickle):
     def step(self, a):
         self.do_simulation(a, self.frame_skip)
         ob = self._get_obs()
-        if self.data.qpos[3] > 0.9:
+        if self.data.qpos[3] > 0.8:
             reward = np.array(self.data.qpos[3])
         else:
             reward = np.array(-1)
@@ -65,5 +65,8 @@ class SegwayEnv(MujocoEnv, utils.EzPickle):
         self.set_state(qpos, qvel)
         return self._get_obs()
 
+    def get_all_obs(self):
+        return np.concatenate([self.data.qpos, self.data.qvel, self.data.sensordata]).ravel()
+
     def _get_obs(self):
-        return np.concatenate([self.data.qpos, self.data.qvel]).ravel()
+        return np.array(self.data.sensordata)
